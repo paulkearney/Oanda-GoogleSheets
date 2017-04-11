34d434d63cad92f961d10fb70ed9093941b6cfcb
Paul Kearney <paul.kearney@nordstrom.com>
2017-04-11 09:09:14 -0700

    v0.1

---
+++ OandaHistory.js
@@ -0,0 +1,184 @@
+function onOpen() {
+  SpreadsheetApp.getUi() 
+      .createMenu('Oanda')
+      .addItem('Update', 'update')
+      .addToUi();
+}
+
+function update() {
+  var accountNum = SpreadsheetApp.getActive().getSheetByName('Account').getRange('B100').getValue();
+  updateAccountData(accountNum);
+  updateOpenTrades(accountNum);
+  updateHistory(accountNum);
+  SpreadsheetApp.getActive().getSheetByName('Account').getRange('B9').setValue(new Date());
+}
+
+function updateHistory(accountId) {
+  var formulas = [['=R[0]C[-8]-R[0]C[-9]', '=YEAR(R[0]C[-10])', '=MONTH(R[0]C[-11])', '=DAY(R[0]C[-12])', '=WEEKDAY(R[0]C[-13])', '=WEEKNUM(R[0]C[-14])', '=HOUR(R[0]C[-15])' ]];
+  var sheet = SpreadsheetApp.getActive().getSheetByName('History');
+  writeHistoryHeaders(sheet);
+  
+  var done = false;
+  var lastHistoryId = 0;
+  while (done == false) {
+    var url = 'https://api-fxtrade.oanda.com/v3/accounts/'+accountId+'/trades?state=CLOSED&count=500';
+    if (lastHistoryId > 0) {
+      url = url + '&beforeID=' + lastHistoryId;
+    }
+    var data = fetchJson(url);
+    var rowUpdates = [];
+    var existingIds = sheet.getRange('A:A').getValues().map(function(row) {return parseInt(row[0]);});
+    
+    for (var t = 0; t < data.trades.length; t++) {
+      var trade = data.trades[t];
+      // see if this trade is already in the list
+      if (existingIds.indexOf(parseInt(trade.id)) > -1) {
+        Logger.log('Already have trade id ' + trade.id); 
+        continue; // we already have this trade
+      }
+      rowUpdates.push([trade.id, trade.clientExtensions.id, trade.initialUnits < 0 ? 'SELL':'BUY', getDateFromIso(trade.openTime), getDateFromIso(trade.closeTime),
+                       formatSymbol(trade.instrument), trade.clientExtensions.tag, Math.abs(trade.initialUnits/100000), trade.price, trade.averageClosePrice, trade.realizedPL, getComment(trade),
+                      ]);
+      lastHistoryId = trade.id;
+    }
+        
+    if (rowUpdates.length == 0) {
+      Logger.log('Last fetch resulted in 0 new rows. Exiting loop.');
+      break;
+    }
+    
+    var numLastRow = sheet.getLastRow();
+    //add row(s) at end if necessary
+    var totalRowsNeeded = rowUpdates.length + sheet.getLastRow();
+    if (totalRowsNeeded > sheet.getMaxRows()) {
+      for (var r = sheet.getMaxRows() - totalRowsNeeded; r > 0; r--) sheet.insertRowAfter(sheet.getMaxRows());
+    }
+
+    numLastRow++;
+    for (var r = 0; r < rowUpdates.length; r++) {
+      var thisRow = numLastRow + r;
+      var valueRange = sheet.getRange('A'+thisRow+':L'+thisRow);
+      var formulaRange = sheet.getRange('M'+thisRow+':S'+thisRow);      
+      valueRange.setValues([rowUpdates[r]]);
+      formulaRange.setFormulasR1C1(formulas)
+    }
+  }
+
+  var maxId = Math.max.apply(null, data.trades.map(function(t) {return t.id;}));
+  var minId = Math.min.apply(null, data.trades.map(function(t) {return t.id;}));
+  SpreadsheetApp.getActive().getSheetByName('Account').getRange('B8').setValue(maxId);
+
+  sheet.sort(1, false);
+}
+
+function updateOpenTrades(accountId) {
+  var sheet = SpreadsheetApp.getActive().getSheetByName('OpenTrades');
+  var row = 2;
+  sheet.getRange('A2:L999').clearContent();
+  writeOpenTradeHeaders(sheet);
+  //get open trades for acct
+  var openTrades = getOpenTrades(accountId);
+  for (var ot = 0; ot < openTrades.trades.length; ot++) {
+    var t = openTrades.trades[ot];
+    var range = sheet.getRange('A'+row+':L'+row);
+    var d = getDateFromIso(t.openTime);
+    range.setValues([[t.id, t.clientExtensions.id, t.initialUnits < 0 ? 'SELL' : 'BUY', d, formatSymbol(t.instrument), t.clientExtensions.tag, Math.abs(t.initialUnits/100000), t.price, t.unrealizedPL, 
+                      getComment(t) ,
+                      '=now()-d'+row, '=i'+row+'/Account!$b$1']]);
+    row++;
+  }
+}
+
+function updateAccountData(accountId) {
+  var data = getAccountData(accountId);
+  var sheet = SpreadsheetApp.getActive().getSheetByName('Account');
+  var range = sheet.getRange('B1:B7');
+  range.clearContent();
+  range.setValues([[data.balance],
+                  [data.marginCloseoutNAV],
+                  [data.marginUsed],
+                  [''],
+                  [data.marginAvailable],
+                  [data.unrealizedPL],
+                  [data.openTradeCount]])
+}
+
+function getAccountData(accountId) {
+  var data = fetchJson('https://api-fxtrade.oanda.com/v3/accounts/' + accountId);
+  return data.account;
+}
+
+function getAccounts() {
+  var data = fetchJson('https://api-fxtrade.oanda.com/v3/accounts');
+  return data.accounts.map(function(a) {return a.id;});
+}
+
+function getOpenTrades(accountId) {
+  var data = fetchJson('https://api-fxtrade.oanda.com/v3/accounts/' + accountId + '/openTrades');
+  return data;
+}
+
+function writeOpenTradeHeaders(sheet) {
+  var range = sheet.getRange('A1:L1');
+  range.setValues([['Id', 'Order Number', 'Type', 'Open Time', 'Symbol', 'Magic Number', 'Lots', 'Open Price', 'OpenPL', 'Comment', 'Duration', 'PL %']]);
+}
+
+function writeHistoryHeaders(sheet) {
+  var range = sheet.getRange('A1:S1');
+  range.setValues([['Id', 'Order Number', 'Type', 'Open Time', 'Close Time',  'Symbol', 'Magic Number', 'Lots', 'Open Price', 'Close Price', 'Profit', 'Comment', 
+                    'Duration', 'Year', 'Month', 'Day', 'DOW', 'Week Number', 'Hour']]);
+}
+
+function fetchJson(url) {
+  Logger.log(url);
+  var apiKey = SpreadsheetApp.getActive().getSheetByName('Account').getRange('B99').getValue();
+  var headers = {
+    'Authorization': 'Bearer ' + apiKey
+  };
+  var options = {
+    'method': 'get',
+    'headers': headers
+  };
+  var response = UrlFetchApp.fetch(url, options);
+  var data = JSON.parse(response.getContentText());
+  return data;
+}
+
+
+// http://delete.me.uk/2005/03/iso8601.html
+function getDateFromIso(string) {
+  try{
+    var aDate = new Date();
+    var regexp = "([0-9]{4})(-([0-9]{2})(-([0-9]{2})" +
+        "(T([0-9]{2}):([0-9]{2})(:([0-9]{2})(\\.([0-9]+))?)?" +
+        "(Z|(([-+])([0-9]{2}):([0-9]{2})))?)?)?)?";
+    var d = string.match(new RegExp(regexp));
+
+    var offset = 0;
+    var date = new Date(d[1], 0, 1);
+
+    if (d[3]) { date.setMonth(d[3] - 1); }
+    if (d[5]) { date.setDate(d[5]); }
+    if (d[7]) { date.setHours(d[7]); }
+    if (d[8]) { date.setMinutes(d[8]); }
+    if (d[10]) { date.setSeconds(d[10]); }
+    if (d[12]) { date.setMilliseconds(Number("0." + d[12]) * 1000); }
+    if (d[14]) {
+      offset = (Number(d[16]) * 60) + Number(d[17]);
+      offset *= ((d[15] == '-') ? 1 : -1);
+    }
+    offset -= date.getTimezoneOffset();
+    time = (Number(date) + (offset * 60 * 1000));
+    return new Date(aDate.setTime(Number(time)));
+  } catch(e){
+    return;
+  }
+}
+
+function getComment(trade) {
+  return typeof(trade.clientExtensions.comment) === 'undefined' ? '' : trade.clientExtensions.comment;
+}
+
+function formatSymbol(symbol) {
+  return symbol.replace(/_/g, '');
+}
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # Oanda-GoogleSheets
 A Google Sheet that imports data from Oanda's v20 API for historical analysis
+
+## Purpose
+I created this Google Sheet for a few reasons:
+
+1. Allow me to quickly review my open trades without needing to remote in to my VPS.
+2. Allow me to perform some historical analysis on my trade history.
+3. I like archiving all of my data somewhere.
+
+## Overview
+This sheet will, every 5 minutes while it is open, connect to Oanda and update three worksheets of data:
+
+1. `Account` worksheet: Account overview
+2. `Open Trades` worksheet: Lists all open trades in the account
+3. `History` worksheeet: A list of all closed trades in the account.
+To view the code (and make sure I'm not secretly stealing your credentials or your account), you can view the file `OandaHistory.js` in this repository. You can also, once you've completed the steps below, select the `Tools` menu, then `Script Editor`.
+
+## Getting Started
+In order to use this, you will need:
+
+1. To be an Oanda customer.
+2. To have an Oanda v20 account. This will not work with their older v1 accounts.
+3. An Oanda API Key. To obtain one, head over to https://www.oanda.com/account/tpa/personal_token. Click generate and make note of your API key. **You will not be able to retrieve the API key again once you leave this page. Make a note of the API key and store it in a safe, secure location. Anyone who has access to this key has access to your account. Do not share the key.**
+4. A Google Docs account. If you don't have one, sign up for one at https://docs.google.com.
+
+Next, you will need to copy the Google Sheet into your own Google account.
+
+1. Go to this document: https://docs.google.com/spreadsheets/d/1u39rB8u0aD__9O_aJbHU2wtIgy83h36EXpIZiV_bIAg/
+2. Make a copy into your own account. Select the `File` menu, then Make a Copy. Give the document a name.
+3. Enter your Oanda API Key on the sheet. On the `Account` tab, scroll down to row 99. In the second column, enter the API key you received from Oanda. *This is in row 99 so it is not visible unless you explicitly scroll down. Once set you do not need to change this.*
+4. Enter your Oanda v20 account number on the sheet. On the `Account` tab, scroll down to row 99. In the second column, enter your v20 account number. You can find it by going to https://www.oanda.com/funding/, selecting the account in the list on the left, then copying the `v20 Account Number`. Note this is not your MT4 account number.
+5. Click the `Oanda` menu, then select `Update`. 
+6. You will be prompted to give 'Oanda History' permission to run. Click 'Continue'.
+7. You will see the `Open Trades` and `History` sheets get updated. The first load of history can take a while depending on how much history you have. 
+
+## Updating the Data
+The data automatically refreshes itself every 5 minutes. You can also trigger a manual refresh by clicking the `Oanda` menu and selecting `Update`.
+
+## Troubleshooting
+This sheet has been tested with a few live and demo accounts, but it is possible I didn't account for every case. If you see something strange, it is most likely related to the `History` sheet. If it seems like this data isn't refreshing correctly, you can select all of the data on the `History` sheet by pressing `CTRL+A` (`CMD+A` on a Mac). Then right click and select `Delete Rows X-Y` (`X-Y` will be numbers). This will force the `History` sheet to be repopulated on the next update.
+
+## Issues? Questions?
+If you have any issues or questions, you can get me on TFL365 Slack @pk, or leave an issue on GitHub.
+
+## Future Updates
+Plans for the future:
+
+1. I've modified my robots (Finch) so that, when taking a second trade, it includes the first trade's order number in the comments. I plan to add another sheet that ties the first and second trades together so you can analyze them as a set.
+2. Add support for multiple accounts in one spreadsheet. For now, if you have multiple v20 accounts, you need to create multiple spreadsheets.
